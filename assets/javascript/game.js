dce5adb5ecffb514d19eae8d6936cedbf25e44e9
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-10-09 08:52:28 -0600

    pseudocoding plus starting html

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+//Variables
+var targetNum = 0;
+var crystalNum = [];
+var winCounter = 0;
+var lossCounter = 0;
+var userTotal = 0;
+
+
+
+
+//Functions
+
+//generate random target number
+
+    //print to screen
+
+//generate random number for crytals
+    //for loop to get four numbers
+    //push numbers to array for holding
+
+//generate random crystal pic
+    //for loop to make a crystal for each number in array
+    //assign class
+    //assign number to crystal with data-
+    //print to screen
+
+
+
+//Process
+
+//create onclick event for crystals
+
+//pull assigned number data
+//turn string data into an integer
+//add crystal value to user total
+//check user total to target num
+    //if equal, you win
+        //reset game
+    //if over, you lose
+        //reset game
