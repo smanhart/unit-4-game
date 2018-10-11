9056545c9609915b7cbf874cd4c6ae02dfbde790
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-10-11 12:58:53 -0600

    images w/random values on page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,6 +4,7 @@ var crystalNum = [];
 var winCounter = 0;
 var lossCounter = 0;
 var userTotal = 0;
+var crystalPics = ["Crys_1.jpg", "Crys_2.jpg", "Crys_3.jpg", "Crys_4.jpg"]
 
 
 
@@ -14,41 +15,55 @@ var userTotal = 0;
 function getRandomNum(min, max) {
     targetNum = Math.floor(Math.random() * (max - min)) + min;
 }
- //print to screen
-// $("#numToGuess").text(targetNum);
-
-   
+getRandomNum(19,120)
+$("#numToGuess").text(targetNum);
+console.log(targetNum);
 
 
 //generate random number for crytals
-
-    function getCrytalNum(min, max) {
-        for(var i=0; i<4; i++) {
-        var singleCrys = Math.floor(Math.random() * (max - min)) + min;
-        crystalNum.push(singleCrys);
-        }
+function getCrytalNum(min, max) {
+    //for loop to get four numbers
+    for(var i=0; i<4; i++) {
+    var singleCrys = Math.floor(Math.random() * (max - min)) + min;
+    //push numbers to array for holding
+    crystalNum.push(singleCrys);
     }
-
+}
 getCrytalNum(1,12);
 console.log(crystalNum);
-    //for loop to get four numbers
-    //push numbers to array for holding
+
 
 //generate random crystal pic
-    //for loop to make a crystal for each number in array
-    //assign class
-    //assign number to crystal with data-
-    //print to screen
-
+//for loop to make a crystal for each number in array
+for (var i = 0; i < crystalNum.length; i++) {
+    
+        var randomCrystal = (Math.floor(Math.random()*crystalPics.length)+1);
+        // $("#crystals").html("<img src='assets/images/Crys_" + randomCrystal + ".jpg'")
+        console.log(randomCrystal);
+        var imageCrystal = $("<img>");
+        //assign class
+        imageCrystal.addClass("crystal-image");
+        //add random crystal number to image source
+        imageCrystal.attr("src", "assets/images/Crys_" + randomCrystal + ".jpg");
+        //assign number to crystal with data-
+        imageCrystal.attr("data-crystalvalue", crystalNum[i]);
+         //print to screen
+        $("#crystals").append(imageCrystal);
+        
+}
 
 
 //Process
 
 //create onclick event for crystals
+$(".crystal-image").on("click", function() {
+    var crystalValue = ($(this).attr("data-crystavalue"))
+    console.log(this);
+});
 
 
-getRandomNum(19,120)
-console.log(targetNum);
+
+
 //pull assigned number data
 //turn string data into an integer
 //add crystal value to user total
